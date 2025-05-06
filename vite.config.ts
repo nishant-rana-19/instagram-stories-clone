0386a74aae4300a8ace874d403639c901f2444a0
Nishant Rana <rananishant19@gmail.com>
2025-05-06 15:39:50 +0530

    updated vite config and build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "instagram-stories-clone",
     plugins: [tailwindcss(), react()],
 });
